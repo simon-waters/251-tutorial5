af62c8ce977a8bebcde5e09b29226208b7157b2c
simon-waters <82448029+simon-waters@users.noreply.github.com>
2021-08-10 00:41:57 +1200

    Added testSubtract method

--- src/test/java/jar/TestCalc.java
+++ src/test/java/jar/TestCalc.java
@@ -4,9 +4,15 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestCalc {
+	Calc calc = new Calc();
+
 	@Test
 	public void testAddition() {
-		Calc calc = new Calc();
 		assertEquals(5, calc.add(2, 3));
+	}
+
+	@Test
+	public void testSubtract() {
+		assertEquals(10, calc.subtract(15, 5));
 	}
 }
